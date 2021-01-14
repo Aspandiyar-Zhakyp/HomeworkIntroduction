7721627da7faaf5af22d1def4b7c9646c6da7120
Aspandiyar-Zhakyp <75847712+Aspandiyar-Zhakyp@users.noreply.github.com>
2021-01-14 19:45:51 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeworkIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Жакып Аспандияр!!!");
+
+            Console.WriteLine("Введите число!");
+
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число!");
+
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ваша сумма!");
+
+            Console.WriteLine(a + b);
+
+            Console.ReadLine();
+        }
+    }
+}
